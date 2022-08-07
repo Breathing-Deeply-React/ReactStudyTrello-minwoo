ae86e60d487d1cfb8fb2d5cf444107b8550a7af2
minwoo9629 <minwoo9629@gmail.com>
2022-08-07 22:52:54 +0900

    Feat : 모달 추가 및 읽기 및 수정 버튼 추가

--- src/components/CategoryListItem.js
+++ src/components/CategoryListItem.js
@@ -1,15 +1,45 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
 import styled from "styled-components";
+import Modal from "./modals/Modal";
+import ModalContent from "./modals/ModalContents/ModalContent";
 
 const ItemWrapper = styled.div`
   padding: 12px 20px;
   box-sizing: border-box;
   margin-bottom: 10px;
   width: 100%;
+  cursor: pointer;
   background-color: white;
 `;
 
-const CategoryListItem = ({ itemTitle }) => {
-  return <ItemWrapper>{itemTitle}</ItemWrapper>;
+const CategoryListItem = ({ itemTitle, id, itemId }) => {
+  const [modalState, setModalState] = useState(false);
+  const [typeState, setTypeState] = useState("");
+  const openModal = (type) => {
+    setTypeState(type);
+    setModalState(true);
+  };
+
+  const closeModal = () => {
+    setModalState(false);
+  };
+  return (
+    <ItemWrapper>
+      {modalState === true ? (
+        <Modal
+          isOpen={modalState}
+          close={closeModal}
+          children={<ModalContent id={id} itemId={itemId} type={typeState} />}
+        />
+      ) : null}
+
+      <div onClick={() => openModal("read")}>{itemTitle}</div>
+      <div>
+        <button onClick={() => openModal("write")}>수정버튼</button>
+      </div>
+    </ItemWrapper>
+  );
 };
 
 export default CategoryListItem;
