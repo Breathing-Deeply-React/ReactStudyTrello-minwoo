1c5833b620967df66b3416ddfcbbf1644671a2f9
minwoo9629 <minwoo9629@gmail.com>
2022-08-07 22:54:58 +0900

    Feat : 카드 수정 변수 추가

--- src/store/types/category.js
+++ src/store/types/category.js
@@ -1,4 +1,5 @@
 export const ADD_LIST = "ADD_LIST";
 export const ADD_CARD = "ADD_CARD";
+export const EDIT_CARD = "EDIT_CARD";
 export const REMOVE_LIST = "REMOVE_LIST";
 export const REMOVE_CARD = "REMOVE_CARD";
