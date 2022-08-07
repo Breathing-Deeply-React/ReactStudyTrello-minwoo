5c6b3cc3f13353d5af834d97bc98bd1f7f49e2cd
minwoo9629 <minwoo9629@gmail.com>
2022-08-07 22:50:39 +0900

    Feat : Redux 세팅

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import store from "./store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
